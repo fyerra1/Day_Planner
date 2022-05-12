c03303e8f53d7ff3ad94f995ee0e37d2445a373e
Franky Erra <frankyerra43@gmail.com>
2022-05-12 00:50:05 -0700

    rearranged timeblock in html & updated function to capture user input

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,13 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-var userInput = document.querySelector("#button-addon1").value;
-console.log(userInput);
+var addUserInput = $("#button-addon1")
+addUserInput.on("click", captureInput)
+
+function captureInput(e) {
+  var init = e.target.parentElement;
+  console.log(init);
+  var userInput = init.children[0].value;
+  console.log(userInput);
+  console.log("yes");
+}
