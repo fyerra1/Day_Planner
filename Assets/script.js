eda0b543dbd31761f17206b44e248cea8b5fc846
Franky Erra <frankyerra43@gmail.com>
2022-05-13 13:02:48 -0700

    began code to set and get local storage info & rearranged html code to align with local storage codes

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,24 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-var addUserInput = $("#button-addon1")
+var addUserInput = $(".btn")
 addUserInput.on("click", captureInput)
 
 function captureInput(e) {
+  console.log("yes");
   var init = e.target.parentElement;
   console.log(init);
   var userInput = init.children[0].value;
   console.log(userInput);
-  console.log("yes");
+  var name = init.children[0].id;
+  localStorage.setItem("name", userInput)
 }
+
+function renderStoredInput() {
+  var activity = localStorage.getItem("name")
+  console.log(activity);
+}
+
+renderStoredInput();
+
+
