64265c9de8225468f6d1ed59102b5f42ffb3017a
Franky Erra <frankyerra43@gmail.com>
2022-05-15 11:36:28 -0700

    modified positioning of web content using html & css, rearranged structure of js

--- Assets/script.js
+++ Assets/script.js
@@ -13,6 +13,7 @@ function captureInput(e) {
   var name = init.children[1].id;
   console.log(name);
   localStorage.setItem(name, userInput)
+  
 }
 
 var timeBlocks = $(".description").toArray();
@@ -29,18 +30,20 @@ function renderStoredInput() {
     console.log(savedInputs);
     console.log(matchBlock);
     console.log(textBlocks);
-    textBlocks.textContent = savedInputs;  
+    textBlocks.textContent = savedInputs;
+
 } 
 }
 
-function colorBlocking() {
+function colorBlocking(e) {
 var hour = moment().hour();
 var startHour = 9
 var block = $(".textarea")
 console.log(hour);
 console.log(timeBlocks[0].id);
 
-for (var i = 0; i < 9; i++){
+
+for (var i = 0; i < timeBlocks.length; i++){
   console.log(plannerHour);
   var plannerHour = startHour + i
   if (hour > plannerHour) {
@@ -50,7 +53,7 @@ for (var i = 0; i < 9; i++){
 }
 }
 
-colorBlocking();
 renderStoredInput();
+colorBlocking();
 
 
